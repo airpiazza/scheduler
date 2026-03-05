9b1b74829ca121318c378d5b110e328334e7954c
airpiazza <nick@piazzaonline.com>
2026-03-05 14:42:25 -0600

    delete appointment

--- src/main/java/com/airpiazza/scheduler/SchedulerController.java
+++ src/main/java/com/airpiazza/scheduler/SchedulerController.java
@@ -3,6 +3,7 @@ package com.airpiazza.scheduler;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,17 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SchedulerController {
     @Autowired
-    private AppointmentRespository appointmentRespository;
+    private AppointmentRespository appointmentRepository;
 
     @PostMapping(path = "/appointment")
     public Appointment createAppointment(@RequestBody Appointment appointment) {
-        return appointmentRespository.save(appointment);
+        return appointmentRepository.save(appointment);
     }
 
     @GetMapping(path = "/appointment/{id}")
     public Appointment getAppointmentById(@PathVariable int id) {
-        Optional<Appointment> appointment = appointmentRespository.findById(id);
+        Optional<Appointment> appointment = appointmentRepository.findById(id);
         
         return appointment.orElse(null);
+    }
+
+    @DeleteMapping(path = "/appointment/{id}")
+    public void deleteAppointmentById(@PathVariable int id) {
+        appointmentRepository.deleteById(id);
     }
 }

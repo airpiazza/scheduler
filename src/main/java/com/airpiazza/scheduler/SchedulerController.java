5a2a418f59d36d63bd16bc2763fc5e71f27c8810
airpiazza <nick@piazzaonline.com>
2026-03-05 14:32:41 -0600

    get appointment

--- src/main/java/com/airpiazza/scheduler/SchedulerController.java
+++ src/main/java/com/airpiazza/scheduler/SchedulerController.java
@@ -1,6 +1,10 @@
 package com.airpiazza.scheduler;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +17,12 @@ public class SchedulerController {
     @PostMapping(path = "/appointment")
     public Appointment createAppointment(@RequestBody Appointment appointment) {
         return appointmentRespository.save(appointment);
+    }
+
+    @GetMapping(path = "/appointment/{id}")
+    public Appointment getAppointmentById(@PathVariable int id) {
+        Optional<Appointment> appointment = appointmentRespository.findById(id);
+        
+        return appointment.orElse(null);
     }
 }

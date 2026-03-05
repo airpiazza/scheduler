2a814dac89b84eed7e1ebb2d86cb26c0d656b371
airpiazza <nick@piazzaonline.com>
2026-03-05 13:46:18 -0600

    post operation

--- src/main/java/com/airpiazza/scheduler/SchedulerController.java
+++ src/main/java/com/airpiazza/scheduler/SchedulerController.java
@@ -1,12 +1,17 @@
 package com.airpiazza.scheduler;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SchedulerController {
-    @GetMapping("/")
-    public String index() {
-        return "hello";
+    @Autowired
+    private AppointmentRespository appointmentRespository;
+
+    @PostMapping(path = "/appointment")
+    public Appointment createAppointment(@RequestBody Appointment appointment) {
+        return appointmentRespository.save(appointment);
     }
 }
